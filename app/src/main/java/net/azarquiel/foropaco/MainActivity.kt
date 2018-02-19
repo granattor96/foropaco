f6a10e5d44fdb5145cd2ed076540cbe4726ee785
Agus <35914575+granattor96@users.noreply.github.com>
2018-02-19 10:04:51 +0100

    3º

--- app/src/main/java/net/azarquiel/foropaco/MainActivity.kt
+++ app/src/main/java/net/azarquiel/foropaco/MainActivity.kt
@@ -2,17 +2,27 @@ package net.azarquiel.foropaco
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import net.azarquiel.foropaco.api.foropacoTemasGet
 import net.azarquiel.foropaco.api.foropacoUsersGet
+import net.azarquiel.foropaco.model.Temas
 import net.azarquiel.foropaco.model.Themes
+import net.azarquiel.foropaco.model.Users
 import net.azarquiel.foropaco.model.Usuarios
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG="**Darkness**"
     }
-    private lateinit var UsuariosGET:ArrayList<Usuarios>
-    private lateinit var ThemesGET:ArrayList<Themes>
+    private lateinit var UsuariosGET:ArrayList<Users>
+    private lateinit var ThemesGET:ArrayList<Temas>
+    private lateinit var original:ArrayList<Temas>
     private val foroPacoUsersGet by lazy{
         foropacoUsersGet.create()
     }
@@ -22,6 +32,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadThemes()
+    }
+    private fun loadThemes(){
+        foroPacoThemesGet.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { /*games ->
+                          this.gamesRes = games.applist.apps as ArrayList<Game>
+                          this.original= ArrayList<Game>()
+                          this.original.addAll(gamesRes)
+                          showGames()
+                          progressBar.visibility = View.GONE
+                        */
+                            temas ->
+                            this.ThemesGET= temas.themes.temas as ArrayList<Temas>
+                            this.original= ArrayList<Temas>()
+                            this.original.addAll(ThemesGET)
+                            showTemas()
 
+
+                        },
+                        { error ->
+                            //progressBar.visibility = View.GONE
+                            Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                            Log.e(TAG,error.message)
+                        }
+                )
+    }
+    private fun showTemas(){
+        Log.d(TAG,"temas")
+        Log.d(TAG,ThemesGET.get(0).temas[0].toString())
+        Log.d((TAG,ThemesGET.get(ThemesGET.size-1).toString())
     }
 }
--- app/src/main/java/net/azarquiel/foropaco/model/class.kt
+++ app/src/main/java/net/azarquiel/foropaco/model/class.kt
@@ -3,8 +3,10 @@ package net.azarquiel.foropaco.model
 /**
  * Created by Agustin on 19/02/2018.
  */
-data class Usuarios(val users: Users)
-data class Users(val telefono: String,val nick:String)
+data class Users(val users: Usuarios)
+data class Usuarios(val usuarios: List<Users>)
+data class Usuario(val telefono:String, val nick:String)
 
-data class Themes(val temas: Temas)
-data class Temas(val _id:String, val descripcion:String)
+data class Themes(val themes: Temas)
+data class Temas(val temas:List<Themes>)
+data class Tema(val _id:String,val descripcion:String)
