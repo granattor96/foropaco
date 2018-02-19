81cdfe4b4cd575dc930729068761bf36806bb15f
Agus <35914575+granattor96@users.noreply.github.com>
2018-02-19 09:37:54 +0100

    3º

--- app/src/main/java/net/azarquiel/foropaco/MainActivity.kt
+++ app/src/main/java/net/azarquiel/foropaco/MainActivity.kt
@@ -2,11 +2,26 @@ package net.azarquiel.foropaco
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import net.azarquiel.foropaco.api.foropacoTemasGet
+import net.azarquiel.foropaco.api.foropacoUsersGet
+import net.azarquiel.foropaco.model.Themes
+import net.azarquiel.foropaco.model.Usuarios
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        val TAG="**Darkness**"
+    }
+    private lateinit var UsuariosGET:ArrayList<Usuarios>
+    private lateinit var ThemesGET:ArrayList<Themes>
+    private val foroPacoUsersGet by lazy{
+        foropacoUsersGet.create()
+    }
+    private val foroPacoThemesGet by lazy {
+        foropacoTemasGet.create()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
